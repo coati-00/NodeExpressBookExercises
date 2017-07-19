d47321759bb11b7e65e218966a6a3f1208d880a6
coati-00 <coati.00@gmail.com>
2017-07-19 19:00:41 -0400

    add fortune telling feature for dynamic view

--- travelwebsite/travelwebsite.js
+++ travelwebsite/travelwebsite.js
@@ -5,16 +5,27 @@ var app = express();
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
 
 app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
+
+var fortunes = [
+  "Fortune 1",
+  "Fortune 2",
+  "Fortune 3",
+  "Fortune 4",
+  "Fortune 5",
+];
+
 
 app.get('/', function(req, res){
   res.render('home');
 });
 
 app.get('/about', function(req, res){
-  res.render('about');
+  var randomeFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', {fortune: randomFortune});
 });
 
 app.use(function(req, res){
